fa57b124d860fda08990c905ff4d9ad32bdc7cc7
yuhyeongeun <yuhyeongeun-KOR@gmail.com>
2024-02-14 09:07:54 +0900

    변수 설정 Sidebar 변수 검색 UI

--- src/app/node-designer/component/variables/variableDeck.tsx
+++ src/app/node-designer/component/variables/variableDeck.tsx
@@ -1,22 +1,29 @@
-import { useEffect , useState } from "react";
+import { useState } from "react";
 import SelectBox from "@/app/node-designer/component/selectBox/selectbox"
-import { OPTIONS } from "@/app/node-designer/component/selectBox/options"
-const VariableDeck = ( {deckId , handlingDeleteDeck} : {deckId : string , handlingDeleteDeck : (deckId : string) => void}) => {
+const VariableDeck = ({
+                        varName,
+                        handlingDeleteDeck,
+                      } 
+                      : 
+                      {
+                        varName : string,
+                        handlingDeleteDeck : (varName : string) => void
+                      }) => {
 
-    const [inputValue, setInputValue] = useState<string>(""); // 상태 추가
+    const [inputValue, setInputValue] = useState<string>(varName); 
 
-    useEffect(() => {
-    setInputValue(`$(변수명+${deckId})`);
-    }, [deckId]);
+    const handlingVarName = (newVarName: string) => {
+      setInputValue(newVarName)
+  }
 
-    return (
-      <div className='sidebar-variable-deck'>
-        <div className='sidebar-variable-remove' onClick={() => handlingDeleteDeck(deckId)}>x</div>
-        <input className="sidebar-variable-deck-row" placeholder="변수명" ></input>
-        <SelectBox></SelectBox>
-        <input className="sidebar-variable-deck-row"type="search" placeholder="Enter Value" role="textbox"></input>
-      </div>
-    )
+  return (
+    <div className='sidebar-variable-deck'>
+      <div className='sidebar-variable-remove' onClick={() => handlingDeleteDeck(varName)}>x</div>
+      <input className="sidebar-variable-deck-row" placeholder="변수명" value={inputValue} onChange={e => handlingVarName(e.target.value)}></input>
+      <SelectBox></SelectBox>
+      <input className="sidebar-variable-deck-row" type="text" placeholder="Enter Value" role="textbox"></input>
+    </div>
+  )
 }
 
     
