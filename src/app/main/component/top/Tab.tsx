fe3ca76664a3c43538dc410f47143f43c854bcf5
YS <yunsikpyun@gmail.com>
2024-02-14 17:00:36 +0900

    노드디자이너 탭 삭제 기능 추가

--- src/app/main/component/top/Tab.tsx
+++ src/app/main/component/top/Tab.tsx
@@ -12,38 +12,65 @@ export type TabHeadItem = {
     id: string;
 }
 
+const topMargin : number =10;
+
 function ItemBox(
     {
         id,
         currentTabHeadId,
         setCurrentTabHeadId,
+        deleteTabItem,
         children
     } : {
         id : string,
         currentTabHeadId : string,
         setCurrentTabHeadId : (id : string) => void,
+        deleteTabItem : (id: string) => void,
         children: React.ReactNode
     }
 ) {
+    //노드디자이너 선택
     const handlerOnClick = () => {
         setCurrentTabHeadId(id);
         mainStateCallbackManager.setCurrentPageName(PageName.NODE_DESIGNER);
-    };
+    }
 
+    //노드디자이너 삭제 처리
+    const handlerOnClickX = () => {
+        multiNodeDesignerCallbackManager.deleteNodeDesigner(id);
+        deleteTabItem(id);
+    }
+
+    //선택된 노드디자이너면 화면에 보여준다.
     if(id === currentTabHeadId)
         multiNodeDesignerCallbackManager.showNodeDesigner(id);
 
     return (
-        <li>
-            <button
-             onClick={handlerOnClick} 
-             className={clsx("text-sm px-[5px]",
-             `h-[${mainLayoutSize['topGNB'].height}px] w-[150px]`,
+        <li
+            style={{height: (mainLayoutSize['topGNB'].height - topMargin)}}
+             className={clsx("text-sm px-[5px] border-solid border-1 border-sky-500 hover:bg-yellow-100",
+             `w-[150px]`,
              {"bg-slate-100" : (id !== currentTabHeadId)},
-             {"bg-slate-300" : (id === currentTabHeadId)})}
-            >
-                {children}
-            </button>
+             {"bg-white border-t-4" : (id === currentTabHeadId)})}
+        >
+            <div className="flex">
+                <button
+                 style={{lineHeight: `${mainLayoutSize['topGNB'].height - topMargin}px`, width: 'calc(100% - 30px)' }}
+                 onClick={handlerOnClick}
+                >
+                    {children}
+                </button>
+                <button
+                 style={{lineHeight: `${mainLayoutSize['topGNB'].height - topMargin}px`, width: '30px' }}
+                 className="hover:bg-red-100"
+                 onClick={handlerOnClickX}
+                >
+                    X
+                </button>
+            </div>
+            <div>
+
+            </div>
         </li>
     );
 }
@@ -52,9 +79,10 @@ export function Tab( { items } : { items : TabHeadItem[]} ) {
     const [currentTabHeadId, setCurrentTabHeadId] = useState<string>('');
     const [tabItems, setTabItems] = useState<any[]>(items);
 
+    //노드디자이너 신규 생성한다.
     const addItem = () => {
         const id : string = multiNodeDesignerCallbackManager.addNodeDesigner();
-        if(id === ''){
+        if(id === '') {
             alert('Node id is empty');
             return;
         }
@@ -64,16 +92,40 @@ export function Tab( { items } : { items : TabHeadItem[]} ) {
         mainStateCallbackManager.setCurrentPageName(PageName.NODE_DESIGNER);
     }
 
+    //노드디자이너가 삭제되면 탭헤더도 삭제한다.
+    const deleteTabItem = (id : string) => {
+        let deletedindex = -1;
+        const newList = tabItems.filter((item, index) => {
+            if(item.id !== id) return true;
+            deletedindex = index;
+            return false;
+        });
+
+        setTabItems(newList);
+
+        if(newList.length < 1) {
+            //노드디자이너가 모두 삭제되었다면, 홈으로 페이지를 이동시킨다.
+            mainStateCallbackManager.setCurrentPageName(PageName.HOME);
+        } else {
+            //삭제된 노드디자이너 대신 다른 노드디자이너를 선택해서 표시한다.
+            if(newList.length-1 < deletedindex)
+                setCurrentTabHeadId(newList[newList.length-1].id);
+            else
+                setCurrentTabHeadId(newList[deletedindex].id);
+        }
+    }
+
     return (
-        <ul className="flex space-x-[2px]">
+        <ul className={clsx(`mt-[${topMargin}px] flex space-x-[2px]`)}>
             {
                 tabItems.map((item, index) => {
                     return (
                         <ItemBox
-                        key={index}
-                        id={item.id}
-                        currentTabHeadId={currentTabHeadId}
-                        setCurrentTabHeadId={setCurrentTabHeadId}
+                         key={index}
+                         id={item.id}
+                         currentTabHeadId={currentTabHeadId}
+                         setCurrentTabHeadId={setCurrentTabHeadId}
+                         deleteTabItem={deleteTabItem}
                         >
                             {item.title}
                         </ItemBox>
@@ -82,9 +134,9 @@ export function Tab( { items } : { items : TabHeadItem[]} ) {
             }
             <li>
                 <button
-                    onClick={addItem} 
-                    className={clsx("text-sm px-[5px]",
-                    `h-[${mainLayoutSize['topGNB'].height}px] w-[150px]`)}
+                    onClick={addItem}
+                    className={clsx('text-sm px-[5px] w-[150px]')}
+                    style={{height: mainLayoutSize['topGNB'].height}}
                 >
                 Add
                 </button>
--- src/app/main/config/layoutFrame.tsx
+++ src/app/main/config/layoutFrame.tsx
@@ -5,6 +5,6 @@ export type MainLayoutSize = {
 
 //NodeDesigner Layout 크기 정의
 export const mainLayoutSize : MainLayoutSize = {
-    topGNB : {width: 0, height: 30},
+    topGNB : {width: 0, height: 40},
     project : {width: 300, height: 0}
 }
