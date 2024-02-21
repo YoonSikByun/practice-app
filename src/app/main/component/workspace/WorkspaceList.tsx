bec7df8273aa7d96e24b17ca8e7ca61a187a3281
YS <yunsikpyun@gmail.com>
2024-02-22 08:56:31 +0900

    workspace 작업중

--- src/app/main/component/workspace/WorkspaceList.tsx
+++ src/app/main/component/workspace/WorkspaceList.tsx
@@ -1,23 +1,28 @@
+import '@/app/main/scss/WorkspaceList.scss';
+
 import { InformationCircleIcon, Bars3Icon } from "@heroicons/react/24/outline"
+
 export default function WorkspaceList() {
     return (
-        <div className='w-full h-full m-[9px] bg-red-300'>
+        <div className='workspace-container bg-red-300'>
             {/* 프로젝트 제목 부분 */}
-            <div className='w-full h-[50px] flex flex-row items-center'>
-                <div className="w-[90%]">
-                    <p className='text-2xl ml-3'>Title</p>
+            <div className='project-title'>
+                <div className="title">
+                    <p className='text-2xl'>Title</p>
                 </div>
-                <div className="w-[10%] h-full flex flex-row-reverse leading-[50px] items-center">
-                    <button><Bars3Icon className='leading-[50px] h-7 w-7 mr-3' /></button>
+                <div className="edit">
+                    <button><Bars3Icon className='h-7 w-7' /></button>
                 </div>
             </div>
             {/* 프로젝트 생성 정보 */}
-            <div className='w-full h-[30px] flex flex-row leading-[30px] border-gray-200 border-b-2'>
-                <div><InformationCircleIcon className='h-7 w-7 mr-1' /></div>
-                <div>Create on 2023/02/21 23:35 by hanaTI@mail.com</div>
+            <div className='project-information'>
+                <div className='project-information-inner'>
+                    <div><InformationCircleIcon className='h-5 w-5 mr-1' /></div>
+                    <div>Create on 2023/02/21 23:35 by hanaTI@mail.com</div>
+                </div>
             </div>
-            <div className='h-full w-full'>
-
+            <div className='list'>
+                Workspace list
             </div>
         </div>
     )
