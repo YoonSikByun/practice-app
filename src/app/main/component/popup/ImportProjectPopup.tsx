1fef4f2c17a7dca4fe8c326e5ba98e697f0ed46e
yuhyeongeun <yuhyeongeun-KOR@gmail.com>
2024-02-23 09:16:41 +0900

    프로젝트 불러오기 UI

--- src/app/main/component/popup/ImportProjectPopup.tsx
+++ src/app/main/component/popup/ImportProjectPopup.tsx
@@ -1,17 +1,32 @@
 import DefaultPopup from "@/app/main/component/popup/DefaultPopup"
+import TextEditor from "../textEditor/textEditor";
 
 function Content() {
     //여기에서 팝업 내용을 넣는다.
+    const handleCloseBtn = () => {
+        // setShowLoadDialog(false)
+    }
+    const handleSearchBtn = () => {
+
+    }
+
     return(
-    <div className="bg-red-200 h-full">
-        <p className="bg-blue-100 text-blue-500 text-2xl">프로젝트 가져오기!!!!</p>
-        input : <input type="text" width={150}/>
-    </div>
+        <div>
+            <div className="dialog-load-project-body">
+                <div className="dialog-load-project-label">프로젝트 불러오기</div>
+                <div className="dialog-load-project-filename">프로젝트를 선택하세요</div>
+                <div className="dialog-load-project-btn" onClick={handleSearchBtn}>찾기</div>
+                <div className="dialog-load-project-label">이름</div>
+                <input className="dialog-load-project-filename"></input>
+                <div className="dialog-load-project-label">설명</div>
+            </div>
+            <TextEditor></TextEditor>
+        </div>
     );
 }
 
-const popupWidth : number = 600;
-const popupHeight : number = 400;
+const popupWidth : number = 800;
+const popupHeight : number = 480;
 
 export default function ImportProjectPopup(
     {
